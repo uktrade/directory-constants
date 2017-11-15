99261bc1b546c20f930af464cc8fd55ec7a12eb4
eadpearce <e.pearce@tutanota.com>
2017-11-15 12:00:37 +0000

    Updated to Nov urls

--- directory_constants/constants/urls.py
+++ directory_constants/constants/urls.py
@@ -1,36 +1,35 @@
 GREAT_HOME = 'http://great.gov.uk/'
-GREAT_EXPORT_HOME = 'http://export.great.gov.uk/'
+GREAT_EXPORT_HOME = 'http://great.gov.uk/'
 
 # EXPORTING PERSONAS
-EXPORTING_NEW = 'http://export.great.gov.uk/new/'
-EXPORTING_OCCASIONAL = 'http://export.great.gov.uk/occasional/'
-EXPORTING_REGULAR = 'http://export.great.gov.uk/regular/'
+EXPORTING_NEW = 'http://great.gov.uk/new/'
+EXPORTING_OCCASIONAL = 'http://great.gov.uk/occasional/'
+EXPORTING_REGULAR = 'http://great.gov.uk/regular/'
 
 # GUIDANCE/ARTICLE SECTIONS
-GUIDANCE_MARKET_RESEARCH = 'http://export.great.gov.uk/market-research/'
-GUIDANCE_CUSTOMER_INSIGHT = 'http://export.great.gov.uk/customer-insight/'
-GUIDANCE_FINANCE = 'http://export.great.gov.uk/finance/'
+GUIDANCE_MARKET_RESEARCH = 'http://great.gov.uk/market-research/'
+GUIDANCE_CUSTOMER_INSIGHT = 'http://great.gov.uk/customer-insight/'
+GUIDANCE_FINANCE = 'http://great.gov.uk/finance/'
 GUIDANCE_BUSINESS_PLANNING = (
-    'http://export.great.gov.uk/business-planning/')
-GUIDANCE_GETTING_PAID = 'http://export.great.gov.uk/getting-paid/'
+    'http://great.gov.uk/business-planning/')
+GUIDANCE_GETTING_PAID = 'http://great.gov.uk/getting-paid/'
 GUIDANCE_OPERATIONS_AND_COMPLIANCE = (
-    'http://export.great.gov.uk/operations-and-compliance/')
+    'http://great.gov.uk/operations-and-compliance/')
 
 # SERVICES
 SERVICES_EVENTS = 'http://events.trade.gov.uk/'
 SERVICES_EXOPPS = 'http://great.gov.uk/export-opportunities/'
 SERVICES_FAB = 'http://find-a-buyer.export.great.gov.uk/'
-SERVICES_GET_FINANCE = 'http://export.great.gov.uk/get-finance/'
+SERVICES_GET_FINANCE = (
+    'http://great.gov.uk/finance/get-finance-support-from-government/')
 SERVICES_SOO = 'http://selling-online-overseas.export.great.gov.uk/'
 
 # INFO
 INFO_CONTACT_US_DIRECTORY = 'https://contact-us.export.great.gov.uk/directory'
 INFO_CONTACT_US_SSO = 'https://contact-us.export.great.gov.uk/single_sign_on'
-INFO_TERMS_AND_CONDITIONS = (
-    'https://www.export.great.gov.uk/terms-and-conditions/')
-INFO_ABOUT = 'http://export.great.gov.uk/about/'
-INFO_PRIVACY_AND_COOKIES = (
-    'http://export.great.gov.uk/privacy-and-cookies/')
+INFO_TERMS_AND_CONDITIONS = 'https://www.great.gov.uk/terms-conditions/'
+INFO_ABOUT = 'http://great.gov.uk/about/'
+INFO_PRIVACY_AND_COOKIES = 'http://great.gov.uk/privacy-cookies/'
 INFO_DIT = (
     'https://www.gov.uk/government/organisations/'
     'department-for-international-trade')

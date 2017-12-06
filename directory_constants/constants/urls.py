49b57dae307675f3221125e9f6d957bbe7ae13b3
Edie Pearce <e.pearce@tutanota.com>
2017-12-06 11:50:14 +0000

    Updated to Nov urls (#37)
    * Updated to Nov urls
    
    * Updated version
    
    * Update urls. Use https
    
    * Updated to new great international page url

--- directory_constants/constants/urls.py
+++ directory_constants/constants/urls.py
@@ -1,36 +1,37 @@
-GREAT_HOME = 'https://great.gov.uk/'
-GREAT_EXPORT_HOME = 'https://export.great.gov.uk/'
+GREAT_HOME = 'https://great.gov.uk/international/'
+GREAT_EXPORT_HOME = 'https://great.gov.uk/'
 
 # EXPORTING PERSONAS
-EXPORTING_NEW = 'https://export.great.gov.uk/new/'
-EXPORTING_OCCASIONAL = 'https://export.great.gov.uk/occasional/'
-EXPORTING_REGULAR = 'https://export.great.gov.uk/regular/'
+EXPORTING_NEW = 'https://great.gov.uk/new/'
+EXPORTING_OCCASIONAL = 'https://great.gov.uk/occasional/'
+EXPORTING_REGULAR = 'https://great.gov.uk/regular/'
 
 # GUIDANCE/ARTICLE SECTIONS
-GUIDANCE_MARKET_RESEARCH = 'https://export.great.gov.uk/market-research/'
-GUIDANCE_CUSTOMER_INSIGHT = 'https://export.great.gov.uk/customer-insight/'
-GUIDANCE_FINANCE = 'https://export.great.gov.uk/finance/'
+GUIDANCE_MARKET_RESEARCH = 'https://great.gov.uk/market-research/'
+GUIDANCE_CUSTOMER_INSIGHT = 'https://great.gov.uk/customer-insight/'
+GUIDANCE_FINANCE = 'https://great.gov.uk/finance/'
 GUIDANCE_BUSINESS_PLANNING = (
-    'https://export.great.gov.uk/business-planning/')
-GUIDANCE_GETTING_PAID = 'https://export.great.gov.uk/getting-paid/'
+    'https://great.gov.uk/business-planning/')
+GUIDANCE_GETTING_PAID = 'https://great.gov.uk/getting-paid/'
 GUIDANCE_OPERATIONS_AND_COMPLIANCE = (
-    'https://export.great.gov.uk/operations-and-compliance/')
+    'https://great.gov.uk/operations-and-compliance/')
 
 # SERVICES
 SERVICES_EVENTS = 'https://events.trade.gov.uk/'
 SERVICES_EXOPPS = 'https://great.gov.uk/export-opportunities/'
 SERVICES_FAB = 'https://find-a-buyer.export.great.gov.uk/'
-SERVICES_GET_FINANCE = 'https://export.great.gov.uk/get-finance/'
+SERVICES_GET_FINANCE = (
+    'https://great.gov.uk/get-finance/')
 SERVICES_SOO = 'https://selling-online-overseas.export.great.gov.uk/'
 
 # INFO
-INFO_CONTACT_US_DIRECTORY = 'https://contact-us.export.great.gov.uk/directory'
-INFO_CONTACT_US_SSO = 'https://contact-us.export.great.gov.uk/single_sign_on'
-INFO_TERMS_AND_CONDITIONS = (
-    'https://www.export.great.gov.uk/terms-and-conditions/')
-INFO_ABOUT = 'https://export.great.gov.uk/about/'
-INFO_PRIVACY_AND_COOKIES = (
-    'https://export.great.gov.uk/privacy-and-cookies/')
+INFO_CONTACT_US_DIRECTORY = (
+    'https://contact-us.export.great.gov.uk/directory/FeedbackForm/')
+INFO_CONTACT_US_SSO = (
+    'https://contact-us.export.great.gov.uk/single_sign_on/FeedbackForm/')
+INFO_TERMS_AND_CONDITIONS = 'https://great.gov.uk/terms-and-conditions/'
+INFO_ABOUT = 'https://great.gov.uk/about/'
+INFO_PRIVACY_AND_COOKIES = 'https://great.gov.uk/privacy-and-cookies/'
 INFO_DIT = (
     'https://www.gov.uk/government/organisations/'
     'department-for-international-trade')

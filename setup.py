023cbdca21bad6067150820413aa3d09fd2b17c5
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-12-17 08:32:46 +0000

    add advice to slug

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_constants',
-    version='11.2.0',
+    version='11.3.0',
     url='https://github.com/uktrade/directory-constants',
     license='MIT',
     author='Department for International Trade',

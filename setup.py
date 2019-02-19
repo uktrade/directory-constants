c35c6ea915ec5ec6b57b9f164bff3ddba075051a
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-02-19 13:50:13 +0000

    refactor readiness to great

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_constants',
-    version='12.5.1',
+    version='13.0.0',
     url='https://github.com/uktrade/directory-constants',
     license='MIT',
     author='Department for International Trade',

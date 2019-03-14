7791e03f273ebc4db0903c1bc8f73440849e27e3
eadpearce <e.pearce@tutanota.com>
2019-03-14 12:14:52 +0000

    Add markets link

--- directory_constants/constants/urls.py
+++ directory_constants/constants/urls.py
@@ -72,6 +72,7 @@ ADVICE_PREPARE_FOR_EXPORT_PROCEDURES_AND_LOGISTICS = build_great_url(
     'advice/prepare-for-export-procedures-and-logistics/'
 )
 
+MARKETS = build_great_url('markets/')
 GET_FINANCE = build_great_url('get-finance/')
 GREAT_DOMESTIC_NEWS = build_great_url('news/')
 TERMS_AND_CONDITIONS = build_great_url('terms-and-conditions/')

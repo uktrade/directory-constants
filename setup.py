cee4b0fc7e5dfc27929bb2c302bfded445bb450b
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2020-08-07 14:43:45 +0100

    add exporting constants

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_constants',
-    version='20.13.1',
+    version='20.14.0',
     url='https://github.com/uktrade/directory-constants',
     license='MIT',
     author='Department for International Trade',

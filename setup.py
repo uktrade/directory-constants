0500c49675ec8ad9537922b4f749d86596b32f5f
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2020-10-19 12:31:06 +0100

    update constants

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_constants',
-    version='20.16.0',
+    version='20.17.0',
     url='https://github.com/uktrade/directory-constants',
     license='MIT',
     author='Department for International Trade',

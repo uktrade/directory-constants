12e12940603ad5db04bc17dbce7019e2ccb26f19
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2021-06-24 10:29:32 +0100

    Updated django 3 minor version requirement

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_constants',
-    version='21.0.0',
+    version='21.0.1',
     url='https://github.com/uktrade/directory-constants',
     license='MIT',
     author='Department for International Trade',

2955a4c06bab14116c7447538f08ffd8815f834c
Rafa Garcia <rafa@rafagarcia.info>
2022-04-27 06:39:10 +0100

    Update Django to security-patched version 3.2.13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_constants',
-    version='21.2.0',
+    version='21.3.0',
     url='https://github.com/uktrade/directory-constants',
     license='MIT',
     author='Department for International Trade',
@@ -22,7 +22,7 @@ setup(
     },
     include_package_data=True,
     install_requires=[
-        'django>=2.2.27,<=3.2.12',
+        'django>=2.2.28,<=3.2.13',
     ],
     extras_require={
         'test': [

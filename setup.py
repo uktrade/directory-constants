cbdcbbe4447875685121fcd572b35c9df94f7e60
Tim Patterson <timpatterson@Tim-Pattersons-MacBook-Pro.local>
2023-03-17 12:14:15 +0000

    Update PyPi version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="directory_constants",
-    version="23.0.0",
+    version="23.0.1",
     url="https://github.com/uktrade/directory-constants",
     license="MIT",
     author="Department for Business and Trade",
